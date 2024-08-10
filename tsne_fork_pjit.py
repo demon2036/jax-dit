8f704ddde0818af44d63ead4fb7e194a45c880dc
jtitor <1165137798@qq.com>
2024-08-10 20:22:03 +0800

    commit

--- tsne_fork_pjit.py
+++ tsne_fork_pjit.py
@@ -65,7 +65,7 @@ def test_sharding(rng, params, vae_params, diffusion_sample, vae, shape, class_l
     # image = vae.apply({'params': vae_params}, latent, method=vae.decode).sample
     # return einops.rearrange(image, 'b c h w->b h w c')
 
-    return rng, latent
+    return rng, latent,params, vae_params
 
 
 def create_state():
@@ -147,12 +147,12 @@ def test_convert():
                           vae=vae),
         mesh=mesh,
         in_specs=(PartitionSpec('data'), PartitionSpec(None), PartitionSpec(None)),
-        out_specs=PartitionSpec('data'), )
+        out_specs=(PartitionSpec('data'),PartitionSpec('data'),PartitionSpec(None),PartitionSpec(None)), )
 
     test_sharding_jit = jax.jit(test_sharding_jit)
     print('Here We Go!')
-    for i in range(1):
-        rng, numbers = test_sharding_jit(rng, converted_jax_params, vae_params)
+    for i in range(2):
+        rng, numbers,converted_jax_params, vae_params = test_sharding_jit(rng, converted_jax_params, vae_params)
         b, *_ = rng.shape
         per_process_batch = b // jax.process_count()
         process_idx = jax.process_index()
