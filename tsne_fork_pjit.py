4d3304d0d6d14ed0aa2ded9c5a3c6b0a60901610
jtitor <1165137798@qq.com>
2024-08-10 23:55:00 +0800

    commit

--- tsne_fork_pjit.py
+++ tsne_fork_pjit.py
@@ -6,6 +6,7 @@ import torch
 import tqdm
 from diffusers import FlaxAutoencoderKL
 from flax.core import FrozenDict
+from flax.jax_utils import replicate
 from flax.training.common_utils import shard_prng_key
 from jax.experimental import mesh_utils
 from jax.experimental.shard_map import shard_map
@@ -71,6 +72,31 @@ def test_sharding(rng, params, vae_params, class_label: int, diffusion_sample, v
     # return
 
     return rng, image
+
+
+def test_sharding2(rng, params, vae_params, class_label: int, diffusion_sample, vae, shape, cfg_scale: float = 1.5):
+    new_rng, local_rng, sample_rng = jax.random.split(rng, 3)
+
+    class_labels = jnp.ones((shape[0],), dtype=jnp.int32) * class_label
+    z = jax.random.normal(key=local_rng, shape=shape)
+    z = jnp.concat([z, z], axis=0)
+    y = jnp.array(class_labels)
+    y_null = jnp.array([1000] * shape[0])
+    y = jnp.concat([y, y_null], axis=0)
+    model_kwargs = dict(y=y, cfg_scale=cfg_scale)
+
+    latent = diffusion_sample.ddim_sample_loop(params, z.shape, z, clip_denoised=False, model_kwargs=model_kwargs,
+                                               key=sample_rng, eta=0.0)
+
+    latent, _ = jnp.split(latent, 2, axis=0)
+
+    latent = latent / 0.18215
+    image = vae.apply({'params': vae_params}, latent, method=vae.decode).sample
+    print(image.shape)
+    image = einops.rearrange(image, 'b c h w->b h w c')
+    # return
+
+    return new_rng, image
 
 
 def create_state():
@@ -186,6 +212,62 @@ def test_convert():
                 print(test_sharding_jit._cache_size())
 
 
+def test_convert2():
+    # jax.distributed.initialize()
+    rng = jax.random.key(0)
+
+    class_label = 2
+
+    b, h, w, c = shape = 64, 32, 32, 4
+
+    # rng = jax.random.split(rng, num=jax.local_device_count())
+    # rng = jax.random.split(rng, num=jax.device_count())
+
+    model, converted_jax_params = create_state()
+    diffusion_sample = create_diffusion_sample(model=model, apply_fn=model.forward_with_cfg)
+
+    converted_jax_params = jax.tree_util.tree_map(jnp.asarray, converted_jax_params)
+
+    vae_path = 'stabilityai/sd-vae-ft-mse'
+    vae, vae_params = FlaxAutoencoderKL.from_pretrained(pretrained_model_name_or_path=vae_path, from_pt=True)
+
+    # vae_params = jax.device_put(vae_params, jax.local_devices()[0])
+
+    #     pass
+
+    vae_params = jax.tree_util.tree_map(lambda x: jnp.asarray(np.array(x)), vae_params)
+
+    print(converted_jax_params['x_embedder']['proj']['kernel'].devices())
+    print(vae_params['decoder']['conv_in']['bias'].devices())
+    print(type(converted_jax_params), type(vae_params))
+
+    # vae_params = FrozenDict(vae_params)
+
+    test_sharding_pmap = functools.partial(test_sharding2, shape=shape, diffusion_sample=diffusion_sample,
+                                           class_label=class_label, vae=vae)
+
+    test_sharding_pmap = jax.pmap(test_sharding_pmap)
+
+    converted_jax_params, vae_params = replicate(converted_jax_params), replicate(vae_params)
+    rng = shard_prng_key(rng)
+
+    for label in range(1000):
+
+        for i in tqdm.tqdm(range(20)):
+
+            rng, numbers = test_sharding_pmap(rng, converted_jax_params, vae_params, )
+            b, *_ = rng.shape
+            per_process_batch = b // jax.process_count()
+            process_idx = jax.process_index()
+            local_rng = rng[per_process_batch * process_idx: per_process_batch * (process_idx + 1)]
+
+            if jax.process_index() == 0:
+                print(rng.shape, numbers.shape)
+                print(local_rng)
+                print(local_rng.shape)
+                # print(test_sharding_pmap._cache_size())
+
+
 def show_image(img, i):
     img = img / 2 + 0.5
     print(img.max(), img.min())
@@ -234,7 +316,8 @@ if __name__ == "__main__":
     # model = DiT_S_2_jax(out_channels=c, labels=1000, image_size=h, condition=True)
     # params = model.init(rng, x, t, y, train=True)['params']
     # print(params.keys())
-    test_convert()
+    # test_convert()
+    test_convert2()
     # model, model_params = test_convert()
 
     """
