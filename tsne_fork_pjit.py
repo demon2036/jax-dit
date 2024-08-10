33520ff62c25ea07295b8e38c5e68b9ee2063fea
jtitor <1165137798@qq.com>
2024-08-10 14:09:23 +0800

    commit

--- tsne_fork_pjit.py
+++ tsne_fork_pjit.py
@@ -53,8 +53,9 @@ def test_convert():
 
     # test_sharding_jit = jax.jit(test_sharding, in_shardings=(None, x_sharding), out_shardings=x_sharding)
 
-    test_sharding_jit = shard_map(test_sharding, mesh=mesh, in_specs=PartitionSpec('data'),
-                                  out_specs=PartitionSpec('data'), )
+    test_sharding_jit = shard_map(test_sharding, mesh=mesh, in_specs=PartitionSpec('data'),out_specs=None,
+                                  # out_specs=PartitionSpec('data'),
+                                  )
 
     # jax.config.update('jax_threefry_partitionable', False)
     # f_exe = test_sharding_jit.lower(rng, x).compile()
