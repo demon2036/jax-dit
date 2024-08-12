a086401f17b8cdc0d51072c9bf21d1f9f3e344e2
jtitor <1165137798@qq.com>
2024-08-13 00:00:54 +0800

    commit

--- tsne_fork_pjit.py
+++ tsne_fork_pjit.py
@@ -181,9 +181,11 @@ def test_convert():
 
     vae_params = jax.tree_util.tree_map(lambda x: jnp.asarray(np.array(x)), vae_params)
 
-    print(converted_jax_params['x_embedder']['proj']['kernel'].devices())
-    print(vae_params['decoder']['conv_in']['bias'].devices())
-    print(type(converted_jax_params), type(vae_params))
+    if jax.process_index()==0:
+
+        print(converted_jax_params['x_embedder']['proj']['kernel'].devices())
+        print(vae_params['decoder']['conv_in']['bias'].devices())
+        print(type(converted_jax_params), type(vae_params))
 
     # vae_params = FrozenDict(vae_params)
 
@@ -247,7 +249,8 @@ def test_convert():
         shard_filename,
         maxcount=data_per_shard,
         maxsize=3e10,
-        start_shard=jax.process_index()
+        start_shard=jax.process_index(),
+        verbose=jax.process_index()==0
         # maxsize=shard_size,
     )
 
@@ -261,8 +264,14 @@ def test_convert():
             local_rng = rng[per_process_batch * process_idx: per_process_batch * (process_idx + 1)]
             local_images = images[per_process_batch * process_idx: per_process_batch * (process_idx + 1)]
             local_class_labels = class_labels[per_process_batch * process_idx: per_process_batch * (process_idx + 1)]
-
+            # jnp.array().devices()
+            print(local_images.devices())
             local_images = jax.device_get(local_images * 255)
+            while True:
+                pass
+
+
+
 
             if jax.process_index() == 0:
                 print(local_images.shape, images.shape)
