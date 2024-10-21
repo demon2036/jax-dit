26a251a7c242863ce013fdaa733e26da40d75b8a
jtitor <1165137798@qq.com>
2024-10-21 15:34:22 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -26,7 +26,9 @@ eval "$(~/miniconda3/bin/conda shell.bash hook)"
 pip install -U jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -U flax optax chex webdataset timm wandb
-pip install einops gcsfs==4.51 tensorflow matplotlib diffusers
+pip install einops  tensorflow matplotlib diffusers
+pip install gcsfs==4.51
+
 
 # 3. Install Pillow-SIMD.
 conda install -c conda-forge -y libjpeg-turbo
